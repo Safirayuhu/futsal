1e421fbfc1220974bd4a178799e183c044f64333
franschaniago <franschaniago5@gmail.com>
2017-12-11 14:54:17 +0700

    penambahan jam

--- lapangan.php
+++ lapangan.php
@@ -52,20 +52,20 @@ if(isset($_POST['submit'])){
         <option value="8">08.00</option>
         <option value="9">09.00</option>
         <option value="10">10.00</option>
-        <option value=""></option>
-        <option value=""></option>
-        <option value=""></option>
-        <option value=""></option>
-        <option value=""></option>
-        <option value=""></option>
-        <option value=""></option>
-        <option value=""></option>
-        <option value=""></option>
-        <option value=""></option>
-        <option value=""></option>
-        <option value=""></option>
-        <option value=""></option>
-        <option value=""></option>
+        <option value="11">11.00</option>
+        <option value="12">12.00</option>
+        <option value="13">13.00</option>
+        <option value="14">14.00</option>
+        <option value="15">15.00</option>
+        <option value="16">16.00</option>
+        <option value="17">17.00</option>
+        <option value="18">18.00</option>
+        <option value="19">19.00</option>
+        <option value="20">20.00</option>
+        <option value="21">21.00</option>
+        <option value="22">22.00</option>
+        <option value="23">23.00</option>
+        <option value="24">24.00</option>
     </select>
 
     Jam Selesai
@@ -73,20 +73,20 @@ if(isset($_POST['submit'])){
         <option value="8">08.00</option>
         <option value="9">09.00</option>
         <option value="10">10.00</option>
-        <option value=""></option>
-        <option value=""></option>
-        <option value=""></option>
-        <option value=""></option>
-        <option value=""></option>
-        <option value=""></option>
-        <option value=""></option>
-        <option value=""></option>
-        <option value=""></option>
-        <option value=""></option>
-        <option value=""></option>
-        <option value=""></option>
-        <option value=""></option>
-        <option value=""></option>
+        <option value="11">11.00</option>
+        <option value="12">12.00</option>
+        <option value="13">13.00</option>
+        <option value="14">14.00</option>
+        <option value="15">15.00</option>
+        <option value="16">16.00</option>
+        <option value="17">17.00</option>
+        <option value="18">18.00</option>
+        <option value="19">19.00</option>
+        <option value="20">20.00</option>
+        <option value="21">21.00</option>
+        <option value="22">22.00</option>
+        <option value="23">23.00</option>
+        <option value="24">24.00</option>
     </select>
     <!-- <br>
     <input type="checkbox" name="jam" id="">08.00<br>
